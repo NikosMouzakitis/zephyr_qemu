add1580c7e565262fadd2de2b0c2ec93647ae77f
NikosMouzakitis <mzktsn@gmail.com>
2024-10-12 19:28:52 +0300

    fix blocking issue, example of run
    Signed-off-by: NikosMouzakitis <mzktsn@gmail.com>

--- new_threads/src/main.c
+++ new_threads/src/main.c
@@ -21,45 +21,57 @@ struct printk_data_t {
 	void *fifo_reserved; /* 1st word reserved for use by fifo */
 	uint32_t led;
 	uint32_t cnt;
+	unsigned char character;
 };
 
 K_FIFO_DEFINE(printk_fifo);
 
 
-void blink(uint32_t sleep_ms, uint32_t id)
+void blink(uint32_t sleep_ms, uint32_t id,unsigned char ch)
 {
 	int cnt = 0;
-	int ret;
-	printk("Blink called with id: %d\n", id);
+	printk("Blink called with id: %d and char: %c\n", id, ch);
 	
 
 	while (1) {
-		struct printk_data_t tx_data = { .led = id, .cnt = cnt };
+		struct printk_data_t tx_data = { .led = id, .cnt = cnt, .character = ch };
 
 		size_t size = sizeof(struct printk_data_t);
 		char *mem_ptr = k_malloc(size);
 		__ASSERT_NO_MSG(mem_ptr != 0);
 
 		memcpy(mem_ptr, &tx_data, size);
-
+		printk("---------------> id: %d puts character: %c\n",id, ch);
 		k_fifo_put(&printk_fifo, mem_ptr);
-
 		k_msleep(sleep_ms);
 		cnt++;
+		ch++;
+		printk("---------------> id: %d updated cnt to: %d\n",id, cnt);
+
+		if(cnt == 4) 
+		{
+			printk("stopping thread: %d\n",id);
+			while(1)
+			{
+				//important so the other thread has the chance to be executed!
+				k_msleep(200);
+			}
+
+		}
 	}
 }
 
 void blink0(void)
 {
 	printk("blinky0 initialized\n");
-	blink(2000000,0);
+	blink(8000000,0,'a');
 
 }
 
 void blink1(void)
 {
 	printk("blinky1 initialized\n");
-	blink(4000000,1);
+	blink(2000000,1,'A');
 }
 
 void uart_out(void)
@@ -68,7 +80,7 @@ void uart_out(void)
 	while (1) {
 		printk("uart_out run while()\n");
 		struct printk_data_t *rx_data = k_fifo_get(&printk_fifo, K_FOREVER);
-		printk("Toggled led%d; counter=%d\n", rx_data->led, rx_data->cnt);
+		printk("************ ******************  **************** *****************>Toggled led%d, counter=%d, character= %c\n", rx_data->led, rx_data->cnt, rx_data->character);
 		k_free(rx_data);
 	}
 }
